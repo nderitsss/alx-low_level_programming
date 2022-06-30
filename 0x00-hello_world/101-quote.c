ec5c3c6a2d73c9dbd94c59f9b9bd0c2788f98e6c
nderitsss <imaninderitu14@gmail.com>
2022-06-30 14:32:01 -0700

    Welp

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main starts reading code
+ *
+ * return 0 means is successful
+ */
+int main(void)
+{  
+	char *s = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	while(*s)putchar(*s++);
+
+	return (1);
+}
