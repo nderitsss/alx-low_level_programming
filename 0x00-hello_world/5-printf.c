e000ba55199ec174e35ae2cc85c310cbdf6fbd09
nderitsss <imaninderitu14@gmail.com>
2022-06-30 08:52:34 -0700

    Gmy c code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * Prints the line witha new line at the end
+ *
+ * 
+ */
+int main(){
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return 0;
+}
