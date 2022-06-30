ceef86ff79f1b331f9fa7a1138217a459f9706ba
nderitss <imaninderitu14@gmail.com>
2022-06-30 12:38:40 -0700

    This is hard grand ma

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * If return is 0 =success
+ */
+int main()
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+
+}
+
