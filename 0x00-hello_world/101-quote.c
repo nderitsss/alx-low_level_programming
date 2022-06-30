c7b3349fa098ce0e01a3bb88ee67e9a96cccbb29
nderitsss <imaninderitu14@gmail.com>
2022-06-30 14:38:52 -0700

    Welp

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,11 @@
  * return 0 means is successful
  */
 int main(void)
-{  
+{
 	char *s = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
-	while(*s)putchar(*s++);
+
+	while (*s)
+		putchar (*s++);
 
 	return (1);
 }
