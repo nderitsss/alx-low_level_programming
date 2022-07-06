ebfc05cfd988b39d30b2be2de62a20fd0fe41a3a
nderitsss <imaninderitu14@gmail.com>
2022-07-06 02:07:19 -0700

    Mahn

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return Always (Success) 0
+ *
+ */
+int _abs(int)
+{
+	char
+	_putchar
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return Always (Success) 0
+ *
+ * */
+void jack_bauer(void)
+{
+	int hour, h1, h2, min, min1, min2;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		h1 = hour / 10;
+		h2 = hour %10;
+		for (min = 0; min < 60; min++)
+		{
+			min1 = min / 10;
+			min2 = min % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(min1 + '0');
+			_putchar(min2 + '0');
+			_putchar('\n');
+		}
+		min = 0;
+	}
+}
