d01dd087a48351385d4d1711162b12b0b5699779
nderitsss <imaninderitu14@gmail.com>
2022-07-07 09:16:25 -0700

    Mahn

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-fndef MAIN_H
+#ifndef MAIN_H
 
 #define MAIN_H
 
@@ -9,7 +9,8 @@ fndef MAIN_H
 
 
 void positive_or_negative(int i);
-
-
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
