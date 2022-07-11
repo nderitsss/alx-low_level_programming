d8c4b4d698e167cdf478b0a7b16eeb13deb52b36
nderitsss <imaninderitu14@gmail.com>
2022-07-11 14:11:12 -0700

    bland

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 /**
  * 5-rev_string.c - reverses a string
- *
+ * @s : a string
  * Return ALways 0
  */
 void rev_string(char *s)
 {
 	int i, n;
 
-	n = _strlen(s);
+	while (s[n] != '\0')
+	{
+		n++;
+	}
 	for (i = 0; i < n / 2; i++)
 	{
-		char ch = s[i];
+		char temp = s[i];
 		s[i] = s[n - 1 -i];
-		s[n - 1 -i] = ch;
+		s[n - 1 -i] = temp;
+		n--;
 	}
 }
