2cd2889962e3d99011097235cb2add993cdd5f32
nderitsss <imaninderitu14@gmail.com>
2022-07-11 11:07:19 -0700

    Mahn

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 /**
  * _puts - print a string to stdout
+ * @ str : a string
  *
  * Return Always 0
  */
 void _puts(char *str)
 {
-	str = (char*)str;
+	str = (char *)str;
 	puts(str);
 }
