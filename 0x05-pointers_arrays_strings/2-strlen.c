73c3641a93c20863d64f03faf76e0a9b74404177
nderitsss <imaninderitu14@gmail.com>
2022-07-11 06:07:43 -0700

    Mahn

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - returns the length of the string
+ *
+ * Return Always 0
+ */
+int _strlen(char *s)
+{
+	int p;
+
+	s = (char *) s;
+	p = strlen(s);
+
+	return (p);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+TASK 0:type casting a pointer into an integer
+TASK 1: swaps value of two integers
+
