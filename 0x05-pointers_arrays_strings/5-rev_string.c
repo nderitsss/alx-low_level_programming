ad891cfdada8e3a63b5945fff8f3c27d944649c7
nderitsss <imaninderitu14@gmail.com>
2022-07-13 06:48:47 -0700

    Broad

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,9 @@ void rev_string(char *s)
 	for (i = 0; i < n / 2; i++)
 	{
 		char temp = s[i];
-		s[i] = s[n - 1 -i];
-		s[n - 1 -i] = temp;
+
+		s[i] = s[n - 1 - i];
+		s[n - 1 - i] = temp;
 		n--;
 	}
 }
