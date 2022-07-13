f48d733f0fcd44327af1ad724e8a52f5f25ee2d6
nderitsss <imaninderitu14@gmail.com>
2022-07-13 09:18:49 -0700

    Broad

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	if (strcmp(s1, s2) > 0)
+	{
 		return (15);
+	}
 	else if (strcmp(s1, s2) < 0)
+	{
 		return (-15);
+	}
 	else
+	{
 		return (0);
+	}
 }
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * reverse_array - reverses array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int z;
+
+	for (z = 0;z < n/2; z++)
+	{
+		int temp = a[z];
+		a[z] = a[n - z - 1];
+		a[n - z -1] = temp;
+	}
+}
