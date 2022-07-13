b4663a0986eea41dedaa2d8a9f1c9cb5f7f7e4e3
nderitsss <imaninderitu14@gmail.com>
2022-07-13 08:51:27 -0700

    Boomerang

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - compares tow strings
+ * @s1 : the destination string
+ * @s2 : the string being copied
+ *
+ * Return ALways 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (strcmp(s1, s2) > 0)
+		return (15);
+	else if (strcmp(s1, s2) < 0)
+		return (-15);
+	else
+		return (0);
+}
