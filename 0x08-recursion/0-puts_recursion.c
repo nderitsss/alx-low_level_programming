0d864d1dc8194b0a20f29645b63bfd06d8a35433
nderitsss <imaninderitu14@gmail.com>
2022-07-20 09:26:20 -0700

    Riptide

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts_recursion - prints to std output
+ * @s : the string iin question
+ *
+ * Return: a null value
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
