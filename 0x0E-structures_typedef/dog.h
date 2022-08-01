b5224aa1b66b275adb17ce6bc561974298785569
nderitsss <imaninderitu14@gmail.com>
2022-08-01 13:44:11 -0700

    Blue

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * struct dog-for a dog
+ * @name : a string
+ * @age : an int
+ * @owner : name
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
