05be4a4f1635da9af0f2c2657139d97d72e54412
nderitsss <imaninderitu14@gmail.com>
2022-08-04 19:55:25 -0700

    Kona

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all - sums all te number od an argument
+ * @n : the integers
+ * @... : an ellipse
+ *
+ * Return : Always an int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list pd;
+	int p = n;
+	int i;
+	int sum = 0;
+
+	va_start(pd,n);
+	
+	if ( n == 0)
+	{
+		return 0;
+	}
+
+	for (i = 0;i < p;i++)
+	{
+		sum += va_arg(pd,int);
+	}
+
+	va_end(pd);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+This repository helps me understand variadic functions
+Task 0 : prints the sum of all arumrnts in a function
