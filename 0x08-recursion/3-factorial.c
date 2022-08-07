b161615214babbf3c538959db61cb6f8ac61ddd7
nderitsss <imaninderitu14@gmail.com>
2022-08-06 22:10:52 -0700

    Sunday

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * factorial - factorial
+ * @n : the int factor
+ *
+ * Return: always 0
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 1)
+	{
+		return (- 1);
+	}
+	return (n * factorial(n-1));
+	
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - find prime numbers
+ * @n : the int
+ *
+ * Return ; always an int
+ */
+int is_prime_number(int n)
+{
+
+}
