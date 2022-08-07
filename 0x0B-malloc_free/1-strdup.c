26747735a60da830ea0eb0a22c1dd8ee429f8049
nderitsss <imaninderitu14@gmail.com>
2022-08-07 10:52:09 -0700

    Purple

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - copies one string and returns it's pointer
+ * @str : the string being copied
+ *
+ * Returns:Alwaysa char
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	s = malloc(strlen(str) + 1);
+	
+	if (str == NULL)
+		return NULL;
+	strcpy(s,str);
+	return (s);
+}
