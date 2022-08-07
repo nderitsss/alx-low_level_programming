8ebbf0e37366868d7a066d8475f896fc78d74c4c
nderitsss <imaninderitu14@gmail.com>
2022-08-07 10:05:00 -0700

    Red

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+#include  <stdio.h>
+/**
+ * create_array - creates an array with a specific first character
+ * @size : size of array
+ * @c : the first character
+ *
+ * Return: always int
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+
+	s = malloc(sizeof(char) * size);
+	s[0] = c;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+To learn about how to allocate memory the right way using malloc function
