433fdff632bb1d4ca173492aa79a3f61b3de8929
nderitsss <imaninderitu14@gmail.com>
2022-08-15 20:46:34 -0700

    Two strins appending

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,6 +27,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
+	if (s1 == NULL || s2 == NULL)
+	{
+		return ("");
+	}
 	strcpy(copy, s1);
 	copy = strncat(copy, s2, n);
 	return (copy);
