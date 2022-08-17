41d5f3ecf81b81a4fa28435ea78c20c69ead7bc6
nderitsss <imaninderitu14@gmail.com>
2022-08-17 07:41:41 -0700

    prime nums recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,5 +9,9 @@
  */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
 
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,23 @@
  */
 int is_prime_number(int n)
 {
-	int m = n / 2;
-
-	if (is_prime_number(n) / m == 0)
+	int m = 2;
+	
+	if (n == 1)
+	{
+		return (0);
+	}
+	while (m <= n / 2)
+	{
+		if (n % m == 0)
+		{
+			return (0);
+		}
+		if (n % m != 0)
+		{
+			return (1);
+		}
+		m++;
+	}
+	return (is_prime_number(n));
 }
