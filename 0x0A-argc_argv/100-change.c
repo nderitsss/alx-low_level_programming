fb072777346fc3ab5e3400c301604ae3390fa5b7
nderitsss <imaninderitu14@gmail.com>
2022-08-16 21:35:48 -0700

    Correcting a few things

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,9 +10,9 @@
 int main(int argc, char *argv[])
 {
 	int count = 0;
-	int d = atoi(argv[2]);
+	int d = atoi(argv[1]);
 
-	if (argc != 2)
+	if (argc != 1)
 	{
 		printf("Error\n");
 		return (1);
@@ -28,25 +28,23 @@ int main(int argc, char *argv[])
 			count += d / 25;
 			d = d % 25;
 		}
-		else if (d / 10 > 0)
+		if (d / 10 > 0)
 		{
 			count += d / 10;
 			d = d % 10;
 		}
-		else if (d / 5 > 0)
+		if (d / 5 > 0)
 		{
 			count += d / 5;
 			d = d % 5;
 		}
-		else if (d / 2 > 0)
+		if (d / 2 > 0)
 		{
 			count += d / 2;
 			d = d % 2;
 		}
-		else
-		{
-			count += d;
-		}
+		count += d;
 	}
+	printf("%d\n", count);
 	return (count);
 }
