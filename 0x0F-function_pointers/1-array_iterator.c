1d394d5b8e69cb41e3d136af3c3ce5d02e839480
nderitsss <imaninderitu14@gmail.com>
2022-08-18 20:44:28 -0700

    Printing an array of ints

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * array_iterator - prints each element of an array
+ * @array : an array of ints
+ * @size : size of array
+ * @action : a pointer to a function that returns nothing
+ *
+ * Return:nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+	int p = size;
+
+	while (i < p)
+	{
+		action(array[i]);
+		i++;
+	}
+}
