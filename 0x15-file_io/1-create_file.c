4d2f58e95477a280b42de382bf6040e0e6de3942
nderitsss <imaninderitu14@gmail.com>
2022-08-22 18:42:32 -0700

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 
 	fo = open(filename, O_CREAT, 600);
 
-	write(fo, text_content,strlen(text_content));
+	write(fo, text_content, strlen(text_content));
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
 	close(fo);
-	return (fo);
+	return (1);
 
 }
