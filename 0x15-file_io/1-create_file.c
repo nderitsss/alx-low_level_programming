f28d16e2f62e4c81e1b91e43ee59a2db2e97eb1a
nderitsss <imaninderitu14@gmail.com>
2022-08-22 18:40:02 -0700

    Creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * create_file - creates a file
+ * @filename: the file name
+ * @text_content: file content
+ *
+ * Return: value of fo
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fo;
+
+	fo = open(filename, O_CREAT, 600);
+
+	write(fo, text_content,strlen(text_content));
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	close(fo);
+	return (fo);
+
+}
