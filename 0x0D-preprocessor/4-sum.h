e3a81e09cfcbe58f212c63617bb868851f15fc0c
nderitsss <imaninderitu14@gmail.com>
2022-08-26 03:06:13 -0700

    Sum of two int

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * sum - sum of two numbers
+ * @a : first int
+ * @b : second int
+ *
+ * Return: sum of two ints
+ */
+int SUM(int a, int b)
+{
+	return (a + b);
+}
+#endif
