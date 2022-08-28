b1b7de6b6f7077ff7da932e797a1cceed29a1794
nderitsss <imaninderitu14@gmail.com>
2022-08-27 20:37:31 -0700

    Prints decimal from bnary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,39 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
-	unsigned int i, k;
+	unsigned int k = 0;
+	unsigned int i;
 	unsigned int count = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	i = atoi(b);
-
 	while (i != 0)
 	{
-	k = pow(2,count);
-	dec += (i % 10) * k;
-	count++;
-	i /= 10;
+		if (i % 10 != 0 || i % 10 != 1)
+		{
+			return (0);
+		}
+		while (i % 10 == 1)
+		{
+			while (k <= count)
+			{
+				if (count == 0)
+				{
+					dec = 1;
+				}
+				if (count == 1)
+				{
+					dec = 2;
+				}
+				dec = dec + 2 * 2;
+				k++;
+			}
+		}
+		count++;
+		i = i / 10;
 	}
 	return (dec);
 
