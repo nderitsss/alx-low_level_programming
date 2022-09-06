a581433279464a9076dad457efc652b0be640985
nderitsss <imaninderitu14@gmail.com>
2022-09-06 06:11:36 -0700

    Correcting betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,9 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 
 	if (h == NULL)
-	{
-		printf("[0] (nil)");
-	}
+		count = 0;
 	while (h != NULL)
 	{
-		if (h == NULL)
-			printf("[0] (nil)");
 		printf("[%d] %s\n", h->len, h->str);
 		count++;
 		
